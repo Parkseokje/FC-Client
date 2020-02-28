82d17d489693795e6c667aabce0ad70e3d76c0a6
seokje Park <molla4455@gmail.com>
2020-02-28 10:45:05 +0900

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps: [
+    {
+      name: "FC-Client",
+      script: "./bin/www",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      key: "~/.ssh/orangenamu.pem",
+      user: "ec2-user",
+      host: "ec2-52-79-160-63.ap-northeast-2.compute.amazonaws.com",
+      ref: "origin/master",
+      repo: "git@github.com:Parkseokje/FC-Client.git",
+      path: "/home/ec2-user/FC-mobile",
+      "pre-deploy": "git reset --hard",
+      "post-deploy": "pm2 startOrRestart ecosystem.config.js --env production"
+    }
+  }
+};
